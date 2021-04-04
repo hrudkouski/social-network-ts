18c51181b3e71c024f72d5107083481054bde3b2
hrudkouski <aprilshower19@gmail.com>
2021-04-04 14:17:19 +0300

    creating basic structure (grid)

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,51 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App-wrapper">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+            </header>
+
+            <nav className='App-nav'>
+                <div>
+                    <a href="https://www.google.com/">Profile</a>
+                </div>
+                <div>
+                    <a href="https://www.google.com/">Messages</a>
+                </div>
+                <div>
+                    <a href="https://www.google.com/">News</a>
+                </div>
+                <div>
+                    <a href="https://www.google.com/">Music</a>
+                </div>
+                <div>
+                    <a href="https://www.google.com/">Settings</a>
+                </div>
+                <div>
+                    <a href="https://www.google.com/">Find friends</a>
+                </div>
+            </nav>
+
+            <div className='App-content'>
+                <div>
+                    <img src={logo} className="App-logo" alt="logo"/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
